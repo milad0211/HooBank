039f7b4ec3e95f3caf7dd08a0b3c272cb42f2240
MilaD <milad.mashayekhi1@gmail.com>
2025-06-08 17:31:08 +0330

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./business";
+import Client from "./Client";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export{
+    Navbar,Billing,CardDeal,Business,Client,CTA,Stats,Footer,Testimonials,Hero
+}
